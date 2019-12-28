41f069e1707fc51c95dd0c900215559c9c47b5d7
kaustav <kaustav541@gmail.com>
2019-12-28 16:27:27 +0000

    updated

--- src/main/java/main/StartLoginRegister.java
+++ src/main/java/main/StartLoginRegister.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("repository")
+/*@EnableJpaRepositories("repository")
 @ComponentScan(basePackages = {"model","controller","service"})
-@EntityScan(basePackages = {"model"})
+@EntityScan(basePackages = {"model"})*/
 public class StartLoginRegister {
     public static void main(String[] args){
         SpringApplication.run(StartLoginRegister.class,args);
